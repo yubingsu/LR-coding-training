2f3a790255125a02262487a6c9f4648bbcc935b6
Yubin Chun <163580035+yubingsu@users.noreply.github.com>
2026-01-12 02:31:08 +0900

    Update 입양 시각 구하기（1）.sql

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -4,3 +4,12 @@ FROM ANIMAL_OUTS
 WHERE DATE_FORMAT(DATETIME, '%H') >= 9 AND DATE_FORMAT(DATETIME, '%H') < 20
 GROUP BY DATE_FORMAT(DATETIME, '%H')
 ORDER BY HOUR
+
+
+# 더 깔끔한 코드
+## ver1 DATE_FORMAT
+# WHERE DATE_FORMAT(datetime, '%H') bewteen 09 and 19 (9이상 19이하)
+# %H : 24시간 기준, %h : 12시간 기준
+
+## ver2 HOUR
+# WHERE HOUR(datetime) bewteen 09 and 19
