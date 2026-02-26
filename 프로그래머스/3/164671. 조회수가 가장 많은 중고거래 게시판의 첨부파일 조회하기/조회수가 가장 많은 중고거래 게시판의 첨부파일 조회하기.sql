e5d81e04606b13405034d0f8a51db7fec094819e
Yubin Chun <163580035+yubingsu@users.noreply.github.com>
2026-02-26 23:46:00 +0900

    Fix SQL query to retrieve most viewed attachments

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -5,7 +5,7 @@ with junbeom as
 (select max(views) as k
 from USED_GOODS_BOARD )
 
-select b.
+select CONCAT('/
 from USED_GOODS_BOARD a join USED_GOODS_FILE b on a.BOARD_ID= b.BOARD_ID
 where a.views in(select k from junbeom) ;
     
