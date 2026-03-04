a2414613cf6bc806581a0d9735ee696db72ea09d
Yubin Chun <163580035+yubingsu@users.noreply.github.com>
2026-03-04 14:20:53 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,3 +1,4 @@
+
 -- 서브쿼리까지 쓸 필요 없음
 
 SELECT I.REST_ID, I.REST_NAME, I.FOOD_TYPE, I.FAVORITES, I.ADDRESS, 
@@ -7,7 +8,7 @@ JOIN REST_REVIEW R
 ON I.REST_ID = R.REST_ID
 WHERE I.ADDRESS LIKE '서울%'
 GROUP BY I.REST_ID
-ORDER BY SCORE DESC, I.FAVORITES DESC;
+ORDER BY AVG(R.REVIEW_SCORE) DESC, I.FAVORITES DESC;
 
 -- [참고] '서울%' : (O), '%서울%' : (X)
 -- [이유1] 서울 주소는 항상 문자열 맨 앞이 '서울'로 시작함. '%서울'을 허용한다면, '경기도 고양시 서울로 123'와 같은 이상치 데이터가 포함될 수 있음
