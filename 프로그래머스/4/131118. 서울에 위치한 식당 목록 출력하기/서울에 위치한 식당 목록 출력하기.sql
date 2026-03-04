88b78b89aaa2c9f4d5698385d4b1e282fad6ffa0
Yubin Chun <163580035+yubingsu@users.noreply.github.com>
2026-03-04 14:20:29 +0900

    Update 서울에 위치한 식당 목록 출력하기.sql

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,21 +1,4 @@
-# -- 코드를 입력하세요
-# SELECT 
-#     RI.REST_ID, 
-#     RI.REST_NAME, 
-#     RI.FOOD_TYPE, 
-#     RI.FAVORITES, 
-#     RI.ADDRESS, 
-#     ROUND(AVG(RR.REVIEW_SCORE),2) AS SCORE
-# FROM 
-#     (SELECT *
-#     FROM REST_INFO
-#     WHERE ADDRESS LIKE '%서울%') RI
-# JOIN REST_REVIEW RR
-# ON RI.REST_ID = RR.REST_ID
-# GROUP BY RI.REST_ID
-# ORDER BY AVG(RR.REVIEW_SCORE) DESC, RI.FAVORITES DESC;
-
--- 서브쿼리까지 쓸 필요 없음;;
+-- 서브쿼리까지 쓸 필요 없음
 
 SELECT I.REST_ID, I.REST_NAME, I.FOOD_TYPE, I.FAVORITES, I.ADDRESS, 
 ROUND(AVG(R.REVIEW_SCORE), 2) AS SCORE
@@ -25,3 +8,7 @@ ON I.REST_ID = R.REST_ID
 WHERE I.ADDRESS LIKE '서울%'
 GROUP BY I.REST_ID
 ORDER BY SCORE DESC, I.FAVORITES DESC;
+
+-- [참고] '서울%' : (O), '%서울%' : (X)
+-- [이유1] 서울 주소는 항상 문자열 맨 앞이 '서울'로 시작함. '%서울'을 허용한다면, '경기도 고양시 서울로 123'와 같은 이상치 데이터가 포함될 수 있음
+-- [이유2] 와일드카드가 문자열 맨 앞에 있을 경우, 인덱스로써의 검색 효율 저하됨
