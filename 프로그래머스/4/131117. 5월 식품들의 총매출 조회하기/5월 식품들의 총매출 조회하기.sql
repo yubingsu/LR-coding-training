001576be10ca039ed0bb21c3cfea2260f47d6f27
Yubin Chun <163580035+yubingsu@users.noreply.github.com>
2026-03-06 23:50:49 +0900

    지표 계산 시 데이터 자체의 해석과 고려는 필수 ('매출'구하기)

--- 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
+++ 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
@@ -1,3 +1,5 @@
+-- 매출 : 수량 * 가격!!
+-- 주의 : 입고된 공장은 다르나, 상품명과 상품ID가 같은 상품이 존재했음 (마요네즈). 다른 입고 공장이라도, 같은 상품이므로 SUM()으로 해당 상품의 총수량을 구한 후, 매출을 구한다.  
 SELECT p.PRODUCT_ID, p.PRODUCT_NAME, P.PRICE * SUM(O.AMOUNT) AS TOTAL_SALES
 FROM FOOD_PRODUCT P
     JOIN FOOD_ORDER O 
@@ -5,4 +7,3 @@ FROM FOOD_PRODUCT P
 WHERE DATE_FORMAT(O.PRODUCE_DATE, '%Y-%m') = '2022-05'
 GROUP BY P.PRODUCT_ID
 ORDER BY TOTAL_SALES DESC, P.PRODUCT_ID ASC 
-
