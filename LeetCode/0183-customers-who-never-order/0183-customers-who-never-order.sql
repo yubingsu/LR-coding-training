be7ce7aaf126e9843ce856268cf420abb727e00a
Yubin Chun <163580035+yubingsu@users.noreply.github.com>
2026-04-03 23:51:59 +0900

    Update 0183-customers-who-never-order.sql

--- LeetCode/0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ LeetCode/0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,7 +1,6 @@
-# Write your MySQL query statement below
+
 SELECT name as Customers
 FROM Customers C
-LEFT JOIN Orders O
-ON C.id = O.customerid
+LEFT JOIN Orders O ON C.id = O.customerid
 WHERE O.customerid IS NULL
 
