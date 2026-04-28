1af07eb2af1240d481175376e286ef47a495544c
Yubin Chun <163580035+yubingsu@users.noreply.github.com>
2026-04-28 23:59:42 +0900

    Update 0196-delete-duplicate-emails.sql

--- LeetCode/0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ LeetCode/0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -7,3 +7,14 @@
 DELETE P1
 FROM person P1, person P2
 where P1.email = P2.email AND P1.id > P2.id;
+
+DELETE
+FROM
+person
+WHERE Id NOT IN
+(
+SELECT minid
+FROM
+(SELECT email, min(id) as minid
+FROM Person
+GROUP BY email );
